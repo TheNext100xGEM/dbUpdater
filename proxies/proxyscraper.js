26a8bba9e15d1ecfd004c89f0f3703e3a0790426
encloinc <jelch2002@gmail.com>
2024-01-26 12:10:12 -0600

    feat: started work on new coinranking api and proxyscraper service

---
+++ proxies/proxyscraper.js
@@ -0,0 +1,132 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require('fs')
+const path = require('path')
+const nfetch = require('node-fetch');
+const { HttpProxyAgent } = require('http-proxy-agent');
+const AbortController = globalThis.AbortController || require("abort-controller")
+
+function isValidIpPort(str) {
+    var regex = /^((25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]?\d):([0-5]?[0-9]{1,4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$/;
+    return regex.test(str);
+}
+
+function chunkArray(array, size) {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += size) {
+        chunks.push(array.slice(i, i + size));
+    }
+    return chunks;
+}
+
+function testProxy(proxy) {
+    return new Promise((resolve) => {
+        const controller = new AbortController();
+
+        const url = 'http://cryptorank.io';
+        const options = {
+            agent: new HttpProxyAgent(`http://${proxy}`),
+            signal: controller.signal
+        };
+
+        const timeout = setTimeout(() => {
+            controller.abort();
+        }, 10000);
+        
+        nfetch(url, options)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Response not OK');
+                }
+                return response.text();
+            })
+            .then(() => {
+                resolve({ proxy, status: true });
+            })
+            .catch(() => {
+                resolve({ proxy, status: false });
+            })
+            .finally(() => {
+                clearTimeout(timeout);
+            });
+
+    });
+}
+
+async function testProxies(proxies) {
+    const proxyChunks = chunkArray(proxies, 50);
+    const results = [];
+
+    for (const chunk of proxyChunks) {
+        console.log(`Testing ${chunk.length} proxies...`);
+        const chunkResults = await Promise.all(chunk.map(testProxy));
+        results.push(...chunkResults);
+    }
+
+    return results;
+}
+
+
+const providers = [
+    async () => {
+        const URL = 'https://api.proxyscrape.com/v3/free-proxy-list/get?request=getproxies&protocol=http&anonymity=all&timeout=2300&proxy_format=ipport&format=text';
+
+        const rawResponse = await fetch(URL)
+        const response = await rawResponse.text()
+
+        return response
+    },
+
+    async () => {
+        const URL = 'https://free-proxy-list.net/';
+    
+        const rawResponse = await fetch(URL)
+        const response = await rawResponse.text()
+    
+        const dom = new JSDOM(response);
+    
+    
+        let proxyNodes = Array.from(dom.window.document.getElementsByClassName("table table-striped table-bordered")[0].querySelector('tbody').childNodes)
+        
+
+        return proxyNodes.map(ChildNode => ChildNode.childNodes[0].innerHTML + ':' + ChildNode.childNodes[1].innerHTML).join('\n')
+    },
+
+    async () => {
+        const URL = 'https://hidemy.io/en/proxy-list/';
+    
+        const rawResponse = await fetch(URL)
+        const response = await rawResponse.text()
+    
+        const dom = new JSDOM(response);
+    
+    
+        let proxyNodes = Array.from(dom.window.document.getElementsByClassName('table_block')[0].querySelector('tbody').childNodes)
+        
+
+        return proxyNodes.map(ChildNode => ChildNode.childNodes[0].innerHTML + ':' + ChildNode.childNodes[1].innerHTML).join('\n')
+    }
+]
+
+async function updateProxies(){
+
+    let proxies = await Promise.all(providers.map(provider => provider()))
+    proxies = proxies.join('\n')
+    
+    const proxyList = proxies.split('\n').map(proxy => proxy.trim()).filter(proxy => isValidIpPort(proxy))
+
+    let validProxies = await testProxies(proxyList)
+    validProxies = validProxies.filter(proxy => proxy.status).map(Proxy => Proxy.proxy)
+
+    console.log(validProxies.length)
+    fs.writeFileSync(path.join(__dirname, '../dumps/proxies.json'), JSON.stringify(validProxies, null, 2))
+
+    return validProxies
+    
+}
+
+updateProxies()
+
+module.exports = {
+    updateProxies
+}
---
+++ tests/coinranking_scrape.js
@@ -0,0 +1,78 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+/*
+const extractURL = (html) => {
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+  
+    // Assuming the <a> tag is always within a div with class "sc-d8888a34-0 ewoqBa"
+    const container = document.querySelector('.sc-d8888a34-0.ewoqBa');
+  
+    if (container) {
+        const link = container.querySelector('a');
+        return link ? link.href : "URL not found";
+    } else {
+        return "Container not found";
+    }
+  }
+
+const getRaw = async () => {
+
+    let cr_ranking = await fetch('https://cryptorank.io/price/saga-genesis')
+    let html = await cr_ranking.text()
+
+    const dom = new JSDOM(html);
+
+    const contractAddressesContainer = dom.window.document.getElementsByClassName('contracts')[0]
+    const url = extractURL(contractAddressesContainer.innerHTML);
+    console.log(url)
+}
+*/
+
+const ChainStandards = {
+    "Binance Smart Chain": "BNB",
+}
+
+
+const getCoinInfo = async (tokenName) => {
+    const rawResponse = await fetch('https://api.cryptorank.io/v0/coins/'+tokenName)
+    const response = await rawResponse.json()
+
+    if(!response.data)
+        return {
+            status: false,
+            message: "Coin not found"
+        }
+
+    const { 
+        tokens,
+        links,
+        name
+    } = response.data;
+    
+    const explorerLink = links.find(Link => Link.type === 'explorer')?.value
+
+    let isCustomChain = !tokens[0]
+
+    console.log(explorerLink)
+    if(explorerLink)
+        isCustomChain = !explorerLink.startsWith(tokens[0]?.explorerUrl)
+
+
+    return {
+        status: true,
+        chains: !isCustomChain ? tokens.map(token => token.platformName) : [name],
+        uniqueKey: tokens[0]?.address || name,
+    }
+
+}
+
+
+const start = async () => {
+    const coinInfo = await getCoinInfo('graphlinq-protocol')
+    console.log(coinInfo)
+
+}
+
+start()
