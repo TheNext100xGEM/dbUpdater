76d311563a6fbf6fe543c5b9f677cd883155e68e
encloinc <jelch2002@gmail.com>
2024-01-30 07:50:08 -0600

    feat: added custom proxy server

--- functions/proxyscraper.js
+++ functions/proxyscraper.js
@@ -28,7 +28,7 @@ function chunkArray(array, size) {
 function testProxy(proxy) {
     return new Promise((resolve, reject) => {
         const [proxyHost, proxyPort] = proxy.split(':');
-        const targetUrl = 'https://cryptorank.io/'; 
+        const targetUrl = 'https://cdn.mailverified.net'; 
         let req;
 
         const agent = new HttpsProxyAgent('http://' + proxyHost + ':' + proxyPort);
@@ -215,12 +215,12 @@ async function updateProxies(){
 /*
 async function test(){
 
-    const res = await testProxy('34.165.22.141:3128')
+    const res = await testProxy('108.61.75.140:8005')
     console.log(res)
 }
-
-test()
 */
+//test()
+
 module.exports = {
     updateProxies
 }
--- runProxyscraper.js
+++ runProxyscraper.js
@@ -1,15 +1,72 @@
+const fs = require('fs')
+const path= require('path')
+const http = require('http');
+const { HttpsProxyAgent } = require('https-proxy-agent');
 const proxyscraper = require('./functions/proxyscraper.js');
 
+
+let proxies = fs.readFileSync(path.join(__dirname, './dumps/rawProxies.txt'), 'UTF-8').split('\n')
+
+
+// Generate a random integer between min (inclusive) and max (inclusive)
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//SERVICE
 const MINUTES = 10;
 
 async function start() {
 
-
-    await proxyscraper.updateProxies()
+    if(proxies.length <= 1)
+        proxies = await proxyscraper.updateProxies()
+    //await proxyscraper.updateProxies()
     setInterval(async function(){
 
-        await proxyscraper.updateProxies()
+        proxies = await proxyscraper.updateProxies()
     }, MINUTES * 1000 * 60)
 }
 
 start()
+
+
+const getRandomProxy = () => {
+    return proxies[getRandomInt(0, proxies.length - 1)]
+}
+
+
+
+const server = http.createServer((req, res) => {
+    // Handle HTTP requests here...
+});
+
+server.on('connect', (req, clientSocket, head) => {
+    const agent = new HttpsProxyAgent(getRandomProxy());
+
+    // Connect to the target through the proxy
+    const { port, hostname } = new URL(`http://${req.url}`);
+    const options = {
+        host: hostname,
+        port: port,
+        agent: agent,
+    };
+
+    const proxySocket = net.connect(options);
+
+    proxySocket.on('connect', () => {
+        clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                        'Proxy-agent: Node.js-Proxy\r\n' +
+                        '\r\n');
+        proxySocket.write(head);
+        proxySocket.pipe(clientSocket);
+        clientSocket.pipe(proxySocket);
+    });
+
+    // Add error handling for both sockets
+});
+
+const PORT = 8005;
+server.listen(PORT, () => {
+    console.log(`Proxy server listening on port ${PORT}`);
+});
